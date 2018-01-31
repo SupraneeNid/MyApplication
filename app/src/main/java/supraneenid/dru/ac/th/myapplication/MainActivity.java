3c31e4a7b0c01234513155a0abc80da124f4cb50
nidan <supranee.h@dru.ac.th>
2018-01-31 16:30:45 +0700

    Finish 31 Jan 2018

--- app/src/main/java/supraneenid/dru/ac/th/myapplication/MainActivity.java
+++ app/src/main/java/supraneenid/dru/ac/th/myapplication/MainActivity.java
@@ -3,11 +3,22 @@ package supraneenid.dru.ac.th.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import supraneenid.dru.ac.th.myapplication.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (savedInstanceState == null)
+        {getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.contentMainFragment, new MainFragment())
+                .commit();
+
+        }
     }
 }
---
+++ app/src/main/java/supraneenid/dru/ac/th/myapplication/fragment/MainFragment.java
@@ -0,0 +1,29 @@
+package supraneenid.dru.ac.th.myapplication.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import supraneenid.dru.ac.th.myapplication.R;
+
+/**
+ * Created by Com_Sci on 31/1/2561.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+
+
+
+}//Main Class
